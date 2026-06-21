3279294a57969db55e6a4721be8e4f5c6f6b5abc
Sunday <trenton.taylor.email@gmail.com>
2026-06-20 22:00:30 -0500

    sunday: Edit src/pages/AccountPage.jsx

--- src/pages/AccountPage.jsx
+++ src/pages/AccountPage.jsx
@@ -111,7 +111,9 @@ export default function AccountPage() {
 
       <PageSection>
         <Card title="Equity Curve" action={<span className="text-xs text-text-faint">Last 30 days</span>}>
-          <EquityCurveChart data={equityCurve} height={300} showYAxis />
+          <ChartInView height={300}>
+            {() => <EquityCurveChart data={equityCurve} height={300} showYAxis />}
+          </ChartInView>
         </Card>
       </PageSection>
 
